756a9c8f213b3b8a2eca93fb9ad459b17c43a40f
shunsei <k.shunsei1227@gmail.com>
2024-02-24 00:17:36 +0900

    error handling of HTTP request

--- src/atcoder_gacha_bot/app.py
+++ src/atcoder_gacha_bot/app.py
@@ -9,39 +9,43 @@ from slack_bolt import App
 
 load_dotenv(find_dotenv())
 
-# ボットトークンとソケットモードハンドラを使ってアプリを初期化
+# ボットトークンを使ってアプリを初期化
 app = App(token=os.getenv('SLACK_BOT_TOKEN'))
 
 @app.message('hello')
-def message_hello(message, say):
+def handle_msg_hello(msg, say):
     say(
         blocks=[
             {
                 'type': 'section',
                 'text': {
                     'type': 'mrkdwn',
-                    'text': f"Hey there <@{message['user']}>!"
+                    'text': f"Hey there <@{msg['user']}>!"
                 }
             }
         ],
         # NOTE:text引数は常に指定が推奨
-        text=f"Hey there <@{message['user']}>!"
+        text=f"Hey there <@{msg['user']}>!"
     )
 
 
 @app.command('/gacha')
-def handle_gacha_command(args):
+def handle_gacha_cmd(args):
     args.ack()
 
     problem = select_problem()
+    if problem is None:
+        args.say("問題の取得に失敗しました")
+        return
+
     diff = get_problem_diff(problem['id'])
-    if diff is not None:
-        diff = diff.get('difficulty', 'unknown')
 
     title = f"{problem['contest_id'].upper()} / {problem['title']}"
-    point = int(problem['point']) if problem['point'] else 'unknown'
+    point = problem['point']
+    if point is not None:
+        point = int(point)
     url = f"https://atcoder.jp/contests/{problem['contest_id']}/tasks/{problem['id']}"
-
+    
     args.say(
         blocks=[
             {
@@ -80,18 +84,30 @@ def handle_gacha_command(args):
 
 
 def select_problem():
-    # FIXME:正常なレスポンスが返ってくる前提
     response = requests.get(os.getenv('AP_PROBLEM_URL'))
+    if not check_status_code(response):
+        return None
+
     prob_list = response.json()
     rand_prob = choice(prob_list)
     return rand_prob
 
 
 def get_problem_diff(prob_id: str):
-    # FIXME:正常なレスポンスが返ってくる前提
     response = requests.get(os.getenv('AP_DIFF_URL'))
-    est_diff = response.json()
-    if prob_id in est_diff:
-        return est_diff[prob_id]
+    if not check_status_code(response):
+        return None
+
+    diff_dict = response.json()
+    if prob_id in diff_dict:
+        est_diff = diff_dict[prob_id]
+        return est_diff.get('difficulty', 'unknown')
     else:
         return None
+
+def check_status_code(response: requests.Response):
+    match response.status_code:
+        case 200:
+            return True
+        case _:
+            return False
